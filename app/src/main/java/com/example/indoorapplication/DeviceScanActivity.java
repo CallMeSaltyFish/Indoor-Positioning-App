e9fdbf66bbaa99e58e9b1201f9c7af0e5437467b
Ziyan Wang <ziyan.wang@ni.com>
2019-12-18 10:31:30 +0800

    use new version api

--- app/src/main/java/com/example/indoorapplication/DeviceScanActivity.java
+++ app/src/main/java/com/example/indoorapplication/DeviceScanActivity.java
@@ -3,9 +3,14 @@ package com.example.indoorapplication;
 import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Build;
 import android.os.Handler;
+import android.os.ParcelUuid;
 
 import androidx.annotation.RequiresApi;
 
@@ -15,37 +20,37 @@ import java.util.*;
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class DeviceScanActivity extends ListActivity {
 
-    private final UUID[] DEVICE_UUIDS = {UUID.fromString("01122334-4556-6778-899a-abbccddeeff0"),
-            UUID.fromString("5e9917bd-f3ac-41e6-8226-3fd79f340dc5")};
+    private final String[] DEVICE_UUIDS = {"b8eb94fd-3679-3527-a762-e685a7b603ac"};
+    private final String[] DEVICE_ADDRS = {"F9:C2:6E:7D:8A:7F"};
     private BluetoothAdapter bluetoothAdapter;
-    private boolean mScanning;
+    private BluetoothLeScanner bluetoothScanner;
+    //private boolean mScanning;
     private Handler handler;
-    //private LeDeviceListAdapter leDeviceListAdapter;
-    private BluetoothAdapter.LeScanCallback leScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi,
-                                     byte[] scanRecord) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (true||device.getAddress() == "F9:C2:6E:7D:8A:7F") {
-                                System.out.println(device.getAddress());
-                                System.out.println(device.getUuids());
-                            }
-//                            leDeviceListAdapter.addDevice(device);
-//                            leDeviceListAdapter.notifyDataSetChanged();
-                        }
-                    });
-                }
-            };
+    private List<ScanFilter> scanFilters;
+    private ScanSettings scanSettings;
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            System.out.println("============scan=============");
+            System.out.println(getUUID(result));
+            System.out.println(result.getDevice().getAddress());
+        }
+    };
 
-    // Stops scanning after 10 seconds.
+    // Stops scanning after given seconds.
     private static final long SCAN_PERIOD = 100000;
 
-    public DeviceScanActivity(BluetoothAdapter btAdapter) {
-        bluetoothAdapter = btAdapter;
+    public DeviceScanActivity(BluetoothAdapter adapter) {
+        bluetoothAdapter = adapter;
+        bluetoothScanner = bluetoothAdapter.getBluetoothLeScanner();
         handler = new Handler();
+        scanSettings = new ScanSettings.Builder().build();
+        scanFilters = new ArrayList<>();
+//        for (String uuid : DEVICE_UUIDS)
+//            scanFilters.add(new ScanFilter.Builder().setServiceUuid(new ParcelUuid(UUID.fromString(uuid))).build());
+        for (String addr : DEVICE_ADDRS)
+            scanFilters.add(new ScanFilter.Builder().setDeviceAddress(addr).build());
         scanLeDevice(true);
     }
 
@@ -56,16 +61,24 @@ public class DeviceScanActivity extends ListActivity {
                 @SuppressLint("NewApi")
                 @Override
                 public void run() {
-                    mScanning = false;
-                    bluetoothAdapter.stopLeScan(leScanCallback);
+                    // mScanning = false;
+                    bluetoothScanner.startScan(scanFilters, scanSettings, scanCallback);
+                    //bluetoothScanner.startScan(scanCallback);
                 }
             }, SCAN_PERIOD);
 
-            mScanning = true;
-            bluetoothAdapter.startLeScan(leScanCallback);
+            //mScanning = true;
+            //bluetoothScanner.startScan(scanCallback);
+            bluetoothScanner.startScan(scanFilters, scanSettings, scanCallback);
         } else {
-            mScanning = false;
-            bluetoothAdapter.stopLeScan(leScanCallback);
+            // mScanning = false;
+            bluetoothScanner.stopScan(scanCallback);
         }
+    }
+
+    public String getUUID(ScanResult result) {
+        String UUIDx = UUID
+                .nameUUIDFromBytes(result.getScanRecord().getBytes()).toString();
+        return UUIDx;
     }
 }
