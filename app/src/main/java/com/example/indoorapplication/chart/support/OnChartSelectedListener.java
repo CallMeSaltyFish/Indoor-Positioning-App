d8855e43f5e122ac40964e7a06fbb170651fb0bd
Salted Fish <you@example.com>
2019-12-19 19:37:55 +0800

    add file

---
+++ app/src/main/java/com/example/indoorapplication/chart/support/OnChartSelectedListener.java
@@ -0,0 +1,9 @@
+package com.example.indoorapplication.chart.support;
+
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+public interface OnChartSelectedListener {
+    void onChartSelected(boolean isChartSelected);
+}
