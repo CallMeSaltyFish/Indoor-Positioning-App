7c8affcf851e992cbcc1b84619ac33b6040a1613
Ziyan Wang <ziyan.wang@ni.com>
2020-01-06 10:56:19 +0800

    show data graph

--- app/src/main/java/com/example/indoorapplication/DataChart.java
+++ app/src/main/java/com/example/indoorapplication/DataChart.java
@@ -1,18 +1,62 @@
 package com.example.indoorapplication;
 
-import lecho.lib.hellocharts.model.ChartData;
-import lecho.lib.hellocharts.model.LineChartData;
+import android.graphics.Color;
+import android.view.View;
+import lecho.lib.hellocharts.gesture.ContainerScrollType;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.*;
 import lecho.lib.hellocharts.view.LineChartView;
+
+import java.util.*;
 
 public class DataChart {
     private LineChartData chartData;
     private LineChartView chartView;
-    public DataChart(LineChartView lineChartView){
-        chartView=lineChartView;
+    private Line dataPoints;
+
+    public DataChart(LineChartView lineChartView) {
+        chartView = lineChartView;
         chartData = new LineChartData();
+        dataPoints = new Line();
+        dataPoints.setColor(Color.parseColor("#18B2C6"));
+        dataPoints.setShape(ValueShape.CIRCLE);
+        dataPoints.setCubic(true);
+        dataPoints.setFilled(false);
+        dataPoints.setHasLabels(false);
+        dataPoints.setHasLabelsOnlyForSelected(true);
+        dataPoints.setHasLines(false);//是否用线显示。如果为false 则没有曲线只有点显示
+        dataPoints.setHasPoints(true);
     }
-//    private ChartData query(){
-//        ChartData data=new LineChartData();
-//
-//    }
+
+    public void updateChart(List<PointValue> chartPointValues) {
+        dataPoints.setValues(chartPointValues);
+        chartData.setLines(Arrays.asList(dataPoints));
+
+        //坐标轴
+        Axis axisX = new Axis(); //X轴
+        axisX.setHasTiltedLabels(true);  //X坐标轴字体是斜的显示还是直的，true是斜的显示
+        axisX.setTextColor(Color.WHITE);  //设置字体颜色
+        //axisX.setName("date");  //表格名称
+        axisX.setTextSize(10);//设置字体大小
+        axisX.setMaxLabelChars(8); //最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
+        //axisX.setValues(chartAxisXValues);  //填充X轴的坐标名称
+        chartData.setAxisXBottom(axisX); //x 轴在底部
+        //data.setAxisXTop(axisX);  //x 轴在顶部
+        axisX.setHasLines(true); //x 轴分割线
+
+        // Y轴是根据数据的大小自动设置Y轴上限(在下面我会给出固定Y轴数据个数的解决方案)
+        Axis axisY = new Axis();  //Y轴
+        axisY.setName("");//y轴标注
+        axisY.setTextSize(10);//设置字体大小
+        chartData.setAxisYLeft(axisY);  //Y轴设置在左边
+        //data.setAxisYRight(axisY);  //y轴设置在右边
+
+        //设置行为属性，支持缩放、滑动以及平移
+        chartView.setInteractive(true);
+        chartView.setZoomType(ZoomType.HORIZONTAL);
+        chartView.setMaxZoom((float) 2);//最大方法比例
+        chartView.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
+        chartView.setLineChartData(chartData);
+        chartView.setVisibility(View.VISIBLE);
+    }
 }
--- app/src/main/java/com/example/indoorapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/indoorapplication/ui/notifications/NotificationsFragment.java
@@ -8,9 +8,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.indoorapplication.DataChart;
 import com.example.indoorapplication.R;
 import com.example.indoorapplication.util.Database;
 import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 import java.util.*;
 
@@ -18,20 +20,29 @@ import java.util.*;
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
-    private List<PointValue> pointValues;
+    private DataChart dataChart;
+    //private List<PointValue> pointValues;
+    private LineChartView chartView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        pointValues = new ArrayList<>();
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        chartView = root.findViewById(R.id.data_line_chart);
+        dataChart = new DataChart(chartView);
         updatePointValue();
-        System.out.println(pointValues);
         return root;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePointValue();
+    }
+
     private void updatePointValue() {
+        List<PointValue> pointValues = new ArrayList<>();
         HashMap<Integer, ArrayList<Integer>> map = new Database(getContext()).get();
         for (Map.Entry entry : map.entrySet()) {
             int distance = (int) entry.getKey();
@@ -39,5 +50,6 @@ public class NotificationsFragment extends Fragment {
             for (int rssi : rssiList)
                 pointValues.add(new PointValue(rssi, distance));
         }
+        dataChart.updateChart(pointValues);
     }
 }
