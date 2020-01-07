74ded4c06d0058b37d7df631549927d267d8e4d0
Ziyan Wang <ziyan.wang@ni.com>
2020-01-07 11:19:19 +0800

    use list view

--- app/src/main/java/com/example/indoorapplication/SettingsActivity.java
+++ app/src/main/java/com/example/indoorapplication/SettingsActivity.java
@@ -3,15 +3,22 @@ package com.example.indoorapplication;
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
 
 @SuppressLint("NewApi")
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class SettingsActivity extends AppCompatActivity {
+    private ListView deviceAddrView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        deviceAddrView=findViewById(R.id.device_addr_view);
+        deviceAddrView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, Arrays.asList("1","2","3","4")));
     }
 }
