1ef69480ffb84c10728360cab5ecf6b2c68d7321
Ziyan Wang <ziyan.wang@ni.com>
2020-01-15 17:40:02 +0800

    save data to text file

--- app/src/main/java/com/example/indoorapplication/MainActivity.java
+++ app/src/main/java/com/example/indoorapplication/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
--- app/src/main/java/com/example/indoorapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/indoorapplication/ui/notifications/NotificationsFragment.java
@@ -1,11 +1,13 @@
 package com.example.indoorapplication.ui.notifications;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -15,13 +17,16 @@ import com.example.indoorapplication.util.Database;
 import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.view.LineChartView;
 
+import java.io.*;
 import java.util.*;
 
 
 public class NotificationsFragment extends Fragment {
 
+    private final static String DATA_FILE = "/data.txt";
     private NotificationsViewModel notificationsViewModel;
     private Button dataProcessingButton;
+    private Button dataSavingButton;
     private DataChart dataChart;
     private LineChartView chartView;
     private HashMap<Integer, Integer> points;
@@ -39,6 +44,13 @@ public class NotificationsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 processData();
+            }
+        });
+        dataSavingButton = root.findViewById(R.id.data_saving_button);
+        dataSavingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
             }
         });
         return root;
@@ -73,5 +85,33 @@ public class NotificationsFragment extends Fragment {
         for (Integer rssi : rssiList)
             System.out.println("(" + rssi + ", " + points.get(rssi) + ")");
         System.out.println("process data");
+    }
+
+    private void saveData() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);//Get IO permission
+        File baseDirectory = getContext().getExternalFilesDir(null);
+        if (!baseDirectory.exists())
+            baseDirectory.mkdir();
+
+        OutputStreamWriter wout = null;
+        try {
+            wout = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream(baseDirectory.getPath() + DATA_FILE)));
+            for (Integer distance : points.keySet()) {
+                int rssi = points.get(distance);
+                wout.write(distance + "=" + rssi + '\n');
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (wout != null) {
+                try {
+                    wout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
